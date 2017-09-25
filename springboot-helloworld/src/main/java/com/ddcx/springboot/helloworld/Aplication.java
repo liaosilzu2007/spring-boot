f7c8a6f83ced3a364ab54a40c8475fa2a988bfaa
liaosi <liaosilzu2007@163.com>
2017-09-26 00:14:38 +0800

    #

--- springboot-helloworld/src/main/java/com/ddcx/springboot/helloworld/Aplication.java
+++ springboot-helloworld/src/main/java/com/ddcx/springboot/helloworld/Aplication.java
@@ -1,16 +1,15 @@
 package com.ddcx.springboot.helloworld;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by liaosi on 2017/9/25.
  */
-@SpringBootConfiguration
+@SpringBootApplication
 public class Aplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloController.class, args);
+        SpringApplication.run(Aplication.class, args);
     }
 }
