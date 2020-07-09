317d54e0938a211f9605734309be61afe9892719
liaosilzu2007 <liaosilzu2007@163.com>
2020-07-09 19:10:18 +0800

    # utils

---
+++ uitls/src/main/java/com/lzumetal/springboot/utils/HttpConnectionManager.java
@@ -0,0 +1,110 @@
+package com.lzumetal.springboot.utils;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.protocol.HttpContext;
+
+import javax.net.ssl.SSLContext;
+import java.security.NoSuchAlgorithmException;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author liaosi
+ * @date 2020-07-09
+ */
+public class HttpConnectionManager {
+
+    /* 客户端和服务器建立连接的超时时间 */
+    private static final int CONNECT_TIMEOUT = 2000;
+
+    /* 从连接池获取连接的超时时间*/
+    private static final int CONNECTION_REQUESTT_IMEOUT = 1000;
+
+    /* 客户端从服务器读取数据的超时时间 */
+    private static final int SOCKET_TIMEOUT = 15000;
+
+    private PoolingHttpClientConnectionManager cm;
+
+    private CloseableHttpClient httpClient;
+
+    public static class LazyHolder {
+        static final HttpConnectionManager instance = new HttpConnectionManager();
+    }
+
+    public static HttpConnectionManager getInst() {
+        return LazyHolder.instance;
+    }
+
+    private HttpConnectionManager() {
+        LayeredConnectionSocketFactory sslsf = null;
+        try {
+            sslsf = new SSLConnectionSocketFactory(SSLContext.getDefault());
+        } catch (NoSuchAlgorithmException e) {
+        }
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("https", sslsf)
+                .register("http", new PlainConnectionSocketFactory())
+                .build();
+        cm = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+        cm.setMaxTotal(500);// 每个主机的最大并行链接数
+        cm.setDefaultMaxPerRoute(3000);// 客户端总并行链接最大数
+
+        //开启一个线程，定期清理连接，每隔5秒清理一次
+        ScheduledExecutorService monitorExecutor = Executors.newSingleThreadScheduledExecutor();
+        monitorExecutor.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    // Close expired connections
+                    cm.closeExpiredConnections();
+                    // Optionally, close connections
+                    // that have been idle longer than 30 sec
+                    cm.closeIdleConnections(30, TimeUnit.SECONDS);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 5000, 5000, TimeUnit.MILLISECONDS);
+
+        //创建唯一的HttpClient
+        HttpClientBuilder httpClientBuilder = HttpClients.custom().setConnectionManager(cm);
+        httpClientBuilder.setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy() {
+            @Override
+            public long getKeepAliveDuration(HttpResponse response, HttpContext context) {
+                long keepAlive = super.getKeepAliveDuration(response, context);
+                if (keepAlive == -1) {
+                    // 如果keep-alive值没有由服务器明确设置，那么保持连接持续5秒。
+                    keepAlive = 5000;
+                }
+                return keepAlive;
+            }
+        });
+
+        RequestConfig defaultRequestConfig = RequestConfig.custom()
+                .setSocketTimeout(SOCKET_TIMEOUT)
+                .setConnectTimeout(CONNECT_TIMEOUT)
+                .setConnectionRequestTimeout(CONNECTION_REQUESTT_IMEOUT)
+                .build();
+        httpClient = httpClientBuilder
+                .setDefaultRequestConfig(defaultRequestConfig)
+                .build();
+    }
+
+    CloseableHttpClient getHttpClient() {
+        return httpClient;
+    }
+}
---
+++ uitls/src/main/java/com/lzumetal/springboot/utils/HttpRequest.java
@@ -0,0 +1,179 @@
+package com.lzumetal.springboot.utils;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author liaosi
+ * @date 2020-07-09
+ */
+public class HttpRequest {
+
+    //    private int connectTimeout = 5000;
+//    private int connectionRequestTimeout = 5000;
+//    private int socketTimeout = 5000;
+    private static final String DEFAULT_CHARSET_NAME = StandardCharsets.UTF_8.name();
+    private Map<String, String> headers = new HashMap<>();
+    private Map<String, String> params = new HashMap<>();
+
+    public static HttpRequest create() {
+        return new HttpRequest();
+    }
+
+    public HttpRequest addHead(String key, String value) {
+        headers.put(key, value);
+        return this;
+    }
+
+    public HttpRequest addHeads(Map<String, String> headers) {
+        this.headers.putAll(headers);
+        return this;
+    }
+
+    public HttpRequest setHeads(Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+
+    public HttpRequest addParam(String key, String value) {
+        params.put(key, value);
+        return this;
+    }
+
+    public HttpRequest addParams(Map<String, String> params) {
+        this.params.putAll(params);
+        return this;
+    }
+
+    public HttpRequest setParams(Map<String, String> params) {
+        this.params = params;
+        return this;
+    }
+
+
+    private List<NameValuePair> mapToList(Map<String, String> formParams) {
+        List<NameValuePair> resuList = new ArrayList<NameValuePair>();
+        for (Map.Entry<String, String> entry : formParams.entrySet()) {
+            resuList.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+        }
+        return resuList;
+    }
+
+    public String get(String url, Map<String, String> params) throws IOException {
+        this.params.putAll(params);
+        return get(url);
+    }
+
+    public String get(String url) throws IOException {
+        String urlTarget = url;
+        //参数需要拼接
+        if (params != null && params.size() > 0) {
+            urlTarget = url + "?" + URLEncodedUtils.format(mapToList(params), DEFAULT_CHARSET_NAME);
+        }
+        HttpGet method = new HttpGet(urlTarget);
+        if (headers != null && headers.size() > 0) {
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                method.addHeader(entry.getKey(), entry.getValue());
+            }
+        }
+        return executeMethod(method);
+    }
+
+    public String post(String url, Map<String, String> params) throws IOException {
+        this.params.putAll(params);
+        return post(url);
+    }
+
+    public String post(String url) throws IOException {
+        HttpPost method = new HttpPost(url);
+        if (headers != null && headers.size() > 0) {
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                method.addHeader(entry.getKey(), entry.getValue());
+            }
+        }
+        if (params != null && params.size() > 0) {
+            UrlEncodedFormEntity uefEntity = new UrlEncodedFormEntity(mapToList(params), DEFAULT_CHARSET_NAME);
+            method.setEntity(uefEntity);
+        }
+//        RequestConfig requestConfig = RequestConfig.custom()
+//                .setConnectTimeout(connectTimeout)
+//                .setConnectionRequestTimeout(connectionRequestTimeout)
+//                .setSocketTimeout(socketTimeout)
+//                .build();
+//        method.setConfig(requestConfig);
+        return executeMethod(method);
+    }
+
+    public String postJson(String url, String json) throws IOException {
+        HttpPost method = new HttpPost(url);
+        if (headers != null && headers.size() > 0) {
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                method.addHeader(entry.getKey(), entry.getValue());
+            }
+        }
+        method.setEntity(new StringEntity(json, DEFAULT_CHARSET_NAME));
+        method.setHeader("content-type", "application/json");
+        return executeMethod(method);
+    }
+
+
+    public String postMultipartFile(String url, String fileParamName, MultipartFile multipartFile, Map<String, String> otherParams) throws IOException {
+        HttpPost method = new HttpPost(url);
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create()
+                .setCharset(StandardCharsets.UTF_8)
+                .setMode(HttpMultipartMode.BROWSER_COMPATIBLE)//加上此行代码解决返回中文乱码问题
+                .addBinaryBody(fileParamName, multipartFile.getInputStream(), ContentType.MULTIPART_FORM_DATA, multipartFile.getOriginalFilename());// 文件流
+        for (Map.Entry<String, String> e : otherParams.entrySet()) {
+            builder.addTextBody(e.getKey(), e.getValue());//表单提交其他参数
+        }
+        HttpEntity entity = builder.build();
+        method.setEntity(entity);
+        return executeMethod(method);
+    }
+
+
+    private String executeMethod(HttpUriRequest request) throws IOException {
+        CloseableHttpClient httpClient = HttpConnectionManager.getInst().getHttpClient();
+        // Create a custom response handler
+        ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+            public String handleResponse(
+                    final HttpResponse response) throws IOException {
+                int status = response.getStatusLine().getStatusCode();
+                if (status == HttpStatus.SC_OK) {
+                    HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity, DEFAULT_CHARSET_NAME) : null;
+                } else {
+                    throw new ClientProtocolException("Unexpected response status: " + status);
+                }
+            }
+
+        };
+        return httpClient.execute(request, responseHandler);
+    }
+
+
+}
