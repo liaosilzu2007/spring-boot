0a236aa9b1fa9314869f58d375bee28c9e137bec
liaosi <liaosilzu2007@163.com>
2020-08-16 11:36:43 +0800

    #

--- utils/src/main/java/com/lzumetal/springboot/utils/common/ServiceException.java
+++ utils/src/main/java/com/lzumetal/springboot/utils/common/ServiceException.java
@@ -1,20 +1,44 @@
 package com.lzumetal.springboot.utils.common;
 
+import com.lzumetal.springboot.utils.response.EBaseResponseCode;
+import com.lzumetal.springboot.utils.response.ResponseCode;
+
 /**
  * @author liaosi
  * @date 2020-08-09
  */
-public class ServiceException extends Exception {
+public class ServiceException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
 
     private int code;
 
-    private String msg;
-
-
-
-    public ServiceException(int code, String msg) {
+    public ServiceException(int code, String message) {
+        super(message);
         this.code = code;
-        this.msg = msg;
+    }
+
+
+    public ServiceException(ResponseCode responseCode) {
+        super(responseCode.getMessage());
+        this.code = responseCode.getCode();
+    }
+
+
+    public ServiceException(ResponseCode responseCode, Throwable cause) {
+        super(responseCode.getMessage(), cause);
+        this.code = responseCode.getCode();
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+
+    public static void main(String[] args) {
+        ServiceException exception = new ServiceException(EBaseResponseCode.C401);
+        System.out.println(exception.getCode());
+        System.out.println(exception.getMessage());
     }
 
 
