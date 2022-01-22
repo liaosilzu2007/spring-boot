84b618b5271b81fca84051a844911c77e031c63f
liaosi <liaosilzu2007@163.com>
2022-01-22 18:33:49 +0800

    #

--- utils/src/main/java/com/lzumetal/springboot/utils/FileUtil.java
+++ utils/src/main/java/com/lzumetal/springboot/utils/FileUtil.java
@@ -1,12 +1,50 @@
 package com.lzumetal.springboot.utils;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author liaosi
  * @date 2020-07-09
  */
+@Slf4j
 public class FileUtil {
+
+
+    public static List<String> readFromLogFiles(List<String> filesNames) {
+        List<String> list = new ArrayList<>();
+        for (String filesName : filesNames) {
+            list.addAll(readFromLogFile(filesName));
+        }
+        return list;
+    }
+
+
+    public static List<String> readFromLogFile(String filesName) {
+        List<String> list = new ArrayList<>();
+        File file = new File(filesName);
+        if (file.exists()) {
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String trim = line.trim();
+                    if (StringUtils.isNotEmpty(trim)) {
+                        list.add(trim);
+                    }
+                }
+            } catch (Exception e) {
+                log.error("读取日志文件|异常|", e);
+                throw new RuntimeException("读取日志文件出错");
+            }
+        }
+        return list;
+    }
 
     /**
      * 删除文件夹以及文件夹下所有文件
